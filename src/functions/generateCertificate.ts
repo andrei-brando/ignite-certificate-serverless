70a86e809424c3d19f44a5c7cfd5c2d34b02437b
andrei-brando <albertbrando@hotmail.com>
2021-11-02 10:33:03 -0300

    gera certificado

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,36 @@
+import chromium from "chrome-aws-lambda";
+import fs from "fs";
+import handlebars from "handlebars";
+import path from "path";
 import { document } from "../utils/dynamodbClient";
+import dayjs from "dayjs";
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
 }
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string;
+}
+
+const compile = async (data: ITemplate) => {
+  const filePath = path.join(
+    process.cwd(),
+    "src",
+    "templates",
+    "certificate.hbs"
+  );
+
+  const html = fs.readFileSync(filePath, "utf8");
+
+  return handlebars.compile(html)(data);
+};
 
 export const handle = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
@@ -18,6 +44,46 @@ export const handle = async (event) => {
       },
     })
     .promise();
+
+  const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
+  const medal = fs.readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    date: dayjs().format("DD/MM/YYYY"),
+    grade,
+    name,
+    id,
+    medal,
+  };
+
+  // gera certificado
+  // compilar usando handlebars
+  const content = await compile(data);
+
+  // tranformar em PDF
+
+  const browser = await chromium.puppeteer.launch({
+    headless: true,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+
+  const pdf = await page.pdf({
+    format: "a4",
+    path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+  });
+
+  await browser.close();
+
+  // salvar no s3
 
   return {
     statusCode: 201,
